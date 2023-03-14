37bac873a78cb3956c7e8356e9a7636023d62e85
estupar <127423760+estupar@users.noreply.github.com>
2023-03-13 20:32:15 -0400

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -4,9 +4,10 @@ print("Sudoku Generator Starting Up!")
 # and border spots for pipes.
 horizontal_cell_count = 9
 text_cells_in_ascii_grid = (4 * horizontal_cell_count + 1)
+number_in_cell = 0
 print("-" * text_cells_in_ascii_grid)
 for i in range(9):
-  print("| 0 " * horizontal_cell_count + "|")
+  print(("| " + str(number_in_cell) + " ") * horizontal_cell_count + "|")
   print("-" * text_cells_in_ascii_grid)
 
 print("Sudoku Generator Finished!")
