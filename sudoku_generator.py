e998ac6ea5cf8640c63e9a03648f08c0daf7fe4d
estupar <127423760+estupar@users.noreply.github.com>
2023-03-19 16:46:28 -0400

    Update sudoku_generator.py
    testing the difference  between uploading the file and just editing this, since that keeps confusing me

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -2,12 +2,26 @@ print("Sudoku Generator Starting Up!")
 
 # one for each character position, space margins around them,
 # and border spots for pipes.
+import random
 horizontal_cell_count = 9
-text_cells_in_ascii_grid = (4 * horizontal_cell_count + 1)
-number_in_cell = 0
+text_cells_in_ascii_grid = (4 * horizontal_cell_count +1)
+
 print("-" * text_cells_in_ascii_grid)
-for i in range(9):
-  print(("| " + str(number_in_cell) + " ") * horizontal_cell_count + "|")
-  print("-" * text_cells_in_ascii_grid)
+
+#generate a random number 1-9
+def random_number():
+    cell = random.randint(1, 9)
+    return cell
+
+#make a row of 9 cells with cell walls    
+def row():
+    for i in range(horizontal_cell_count):
+        print("| " + str(random_number()), end = " ")
+    print("|")
+
+#repeat the row 9 times
+for i in range (horizontal_cell_count):
+    row()
+    print("-" * text_cells_in_ascii_grid)
 
 print("Sudoku Generator Finished!")
