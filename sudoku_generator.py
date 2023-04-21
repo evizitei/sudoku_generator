f3a1cf7dcab4f55d15377513c05a2f294296a677
estupar <estupar7@gmail.com>
2023-04-20 23:26:04 -0400

    fix column offset bug

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -5,10 +5,7 @@ text_cells_in_ascii_grid = (4 * horizontal_cell_count +1)
 vertical_border_character = "-"
 
 import random
-import math
 
-# changed to list comprehension that generates
-# a list that contains nine lists of nine zeroes
 sudoku_grid = [[0]*9 for x in range(9)]
 
 def is_conflict_in_row(grid, val, row):
@@ -18,9 +15,6 @@ def is_conflict_in_row(grid, val, row):
     return False
 
 def is_conflict_in_column(grid, val, col):
-    # changed to list comprehension that generates
-    # a list containing all the numebrs at a specific
-    # index for each of the nine rows
     list_for_col = [grid[row_num][col] for row_num in range(9)]
     if val in list_for_col:
         return True
@@ -38,7 +32,7 @@ def get_block_for_coordinates(row, col):
     #  row = 1, col = 0 => block = 1
     #  row = 8, col = 8 => block = 9
     #  row = 4, col = 2 => block = 4
-    return (math.floor(row / 3) * 3 + 1) + math.floor(col / 3)
+    return ((row // 3) * 3 + 1) + (col // 3)
 
 def get_elements_for_sudoku_block(grid, block_number):
     #
@@ -50,12 +44,11 @@ def get_elements_for_sudoku_block(grid, block_number):
     #  grid[3][0:3] + grid[4][0:3] + grid[5][0:3]
     # block 7
     #  grid[6][0:3] + grid[7][0:3] + grid[8][0:3]
-    row_offset = math.floor((block_number - 1) / 3) * 3
-    col_offset = ((block_number % 3) - 1) * 3
+    row_offset = ((block_number - 1) // 3) * 3
+    col_offset = ((block_number - 1) %3) * 3
     return grid[row_offset][col_offset:col_offset + 3] + grid[row_offset + 1][col_offset:col_offset + 3] + grid[row_offset + 2][col_offset:col_offset + 3]
 
 def is_conflict_in_subgrid(grid, val, row, col):
-    # THIS IS WHERE THER WORK WILL
     block_number = get_block_for_coordinates(row, col)
     list_for_subgrid = get_elements_for_sudoku_block(grid, block_number)
     if val in list_for_subgrid:
@@ -63,8 +56,6 @@ def is_conflict_in_subgrid(grid, val, row, col):
     return False
 
 def conflict_exists(grid, val, row, col):
-    # conflict could exist in a ROW, or a COL, or a BLOCK (3x3)
-    # sub-grid.  Need to check all 3.
     if is_conflict_in_row(grid, val, row):
         return True
     if is_conflict_in_column(grid, val, col):
